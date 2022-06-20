2a345f50ff2696a216c235e48b058f59572c3d0c
hyunjin-C <zn9852@naver.com>
2022-06-21 02:17:25 +0900

    update 2609.py (내장되어있는 함수 사용)

--- 수학 1/2609(최대공약수와 최소공배수).py
+++ 수학 1/2609(최대공약수와 최소공배수).py
@@ -1,18 +1,8 @@
-# 유클리드 호제법 이용
+# 내장되어있는 함수 사용
+
+import math
 
 num1, num2 = map(int, input().split())
 
-
-def GCD(x, y):
-    while y > 0:
-        x, y = y, x % y
-
-    return x
-
-
-def LCM(x, y):
-    return x * y // GCD(x, y)
-
-
-print(GCD(num1, num2))
-print(LCM(num1, num2))
+print(math.gcd(num1, num2))
+print(math.lcm(num1, num2))
