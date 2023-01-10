ac01eede3cee22dc429796dec76c9f4bee4b5745
hyunjin-C <zn9852@naver.com>
2023-01-11 03:04:54 +0900

    Update leetcode1.py

--- 배열/leetcode1.py
+++ 배열/leetcode1.py
@@ -1,14 +1,17 @@
 def twoSum(nums, target):
-    # for i in range(len(nums) - 1):
-    #     for j in range(1, len(nums)):
-    #         if nums[i] + nums[j] == target:
-    #             return i, j
+    # 방법 1 (브루트포스)
+    for i in range(len(nums) - 1):
+        for j in range(1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return i, j
 
-    # for i, num1 in enumerate(nums):
-    #     num2 = target - num1
-    #     if num2 in nums[i+1:]:
-    #         return [nums.index(num1), nums[i+1:].index(num2) + (i + 1)]
+    # 방법 2 (in을 이용한 탐색)
+    for i, num1 in enumerate(nums):
+        num2 = target - num1
+        if num2 in nums[i+1:]:
+            return [nums.index(num1), nums[i+1:].index(num2) + (i + 1)]
 
+    # 방법 3 (첫 번째 수 뺀 결과를 키로 조회)
     nums_map = {}
     # 키와 값을 바꿔서 딕셔너리로 저장
     for i, num in enumerate(nums):
@@ -18,7 +21,10 @@ def twoSum(nums, target):
         if target - num in nums_map and i != nums_map[target - num]:
             return [i, nums_map[target - num]]
 
-
-print(twoSum([2, 7, 11, 15], 9))
-print(twoSum([3, 2, 4], 6))
-print(twoSum([3, 3], 6))
+    # 방법 4 (방법 3 개선)
+    nums_map = {}
+    # 키와 값을 바꿔서 딕셔너리로 저장
+    for i, num in enumerate(nums):
+        if target - num in nums_map:
+            return [nums_map[target - num], i]
+        nums_map[num] = i
