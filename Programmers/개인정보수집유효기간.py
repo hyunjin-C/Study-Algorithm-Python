74f570bff421c4f943cd07860570d54c68d1f441
hyunjin-C <zn9852@naver.com>
2023-06-22 17:14:09 +0900

    Create 개인정보수집유효기간.py

--- Programmers/개인정보수집유효기간.py
+++ Programmers/개인정보수집유효기간.py
@@ -2,19 +2,15 @@ def solution(today, terms, privacies):
     answer = []
     terms_dic = {}
 
-    today_year, today_month, today_day = today.split('.')
-    today_year, today_month, today_day = int(
-        today_year), int(today_month), int(today_day)
+    today_year, today_month, today_day = map(int, today.split('.'))
 
     for i in range(len(terms)):
         terms_dic[terms[i][0]] = int(terms[i][2:])
 
     for i in range(len(privacies)):
         type = privacies[i][-1]
-        privacies_year, privacies_month, privacies_day = privacies[i][:-2].split(
-            '.')
-        privacies_year, privacies_month, privacies_day = int(
-            privacies_year), int(privacies_month), int(privacies_day)
+        privacies_year, privacies_month, privacies_day = map(
+            int, privacies[i][:-2].split('.'))
 
         privacies_month += terms_dic[type]
 
@@ -34,3 +30,12 @@ def solution(today, terms, privacies):
                 if today_day >= privacies_day:
                     answer.append(i+1)
     return answer
+
+
+print(solution("2022.05.19", ["A 6", "B 12", "C 3"], [
+      "2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]))
+
+print(solution("2020.01.01", ["Z 3", "D 5"], [
+      "2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"]))
+
+print(solution("2021.12.08", ["A 18"], ["2020.06.18 A"]))
